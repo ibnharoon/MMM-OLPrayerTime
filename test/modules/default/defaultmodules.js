3c026d231d722eed221f573f30ad1e3fbfdcb036
ibnharoon <bustamam@gmail.com>
2024-04-09 10:43:41 -0700

    Update defaultmodules.js

--- test/modules/default/defaultmodules.js
+++ test/modules/default/defaultmodules.js
@@ -1,7 +1,7 @@
 /* Default Modules List
  * Modules listed below can be loaded without the 'default/' prefix. Omitting the default folder name.
  */
-const defaultModules = ["alert", "calendar", "clock", "compliments", "helloworld", "newsfeed", "updatenotification", "weather"];
+const defaultModules = ["clock"];
 
 /*************** DO NOT EDIT THE LINE BELOW ***************/
 if (typeof module !== "undefined") {
