1174e03892e252108c11453f14b971214a51d936
ibnharoon <bustamam@gmail.com>
2024-04-10 19:14:00 -0700

    Update test01.js

--- test/js/test01.js
+++ test/js/test01.js
@@ -4,7 +4,7 @@ const { Builder, By, until } = require('selenium-webdriver');
 // Import exec and util for executing shell commands and util.promisify
 const { exec } = require('child_process');
 const util = require('util');
-const { initializeSeleniumDriver }  = require('lib/utils');
+const { initializeSeleniumDriver }  = require('../lib/utils');
 
 // Convert exec to a promise-based function for async/await usage
 const execAsync = util.promisify(exec);
