81907913cda7f7661ac8a8e0d58c9b1606604f9f
ibnharoon <bustamam@gmail.com>
2024-04-10 18:31:28 -0700

    Update utils.js

--- test/js/lib/utils.js
+++ test/js/lib/utils.js
@@ -1,1 +1,32 @@
+/*
+ * Name: initializeSeleniumDriver
+ *
+ * Parameters:
+ *   url        - URL for the selenium instance 
+ *   retryCount - number of retries 
+ *   interval   - retry interval
+ *
+ * Returns: the selenium driver instance if it's up or null if it ran
+ * out of retries.
+ *
+ */
+async function initializeSeleniumDriver(url, retryCount = 9, interval = 5000) {
+  let attempt = 0;
+  while (attempt < retryCount) {
+    try {
+      // Try to create a driver instance to check if the Selenium server is up
+      const driver = await new Builder()
+        .forBrowser('chrome')
+        .usingServer(url)
+        .build();
 
+      return driver; // Server is up
+    } catch (error) {
+      console.log(`Selenium server not up yet, retrying... (${++attempt}/${retryCount})`);
+      await new Promise(resolve => setTimeout(resolve, interval));
+    }
+  }
+
+  // driver could not be created
+  return null;
+}
