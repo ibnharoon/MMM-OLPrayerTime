9065517b4a37420a34b493cfa35bca2d11595877
ibnharoon <bustamam@gmail.com>
2024-04-30 09:32:33 -0700

    Update utils.js

--- test/lib/utils.js
+++ test/lib/utils.js
@@ -170,7 +170,7 @@ function generateTest(rdate) {
   console.log('midnight next: ' + mtime.toDate());
   const mnnext = pt['Midnight'].time.isAfter(mtime);
   Object.entries(prayertime).forEach(([prayerName, prayerTime]) => {
-  // if (['Maghrib'].includes(prayerName)) {
+  if (['Fajr'].includes(prayerName)) {
     // console.log('dobj: ' + prayerTime);
     var fakeTime = '@' + pt[prayerName].time.tz('America/Los_Angeles').subtract(2, 'minute').format('YYYY-MM-DD HH:mm:ss');
     // console.log('faketime: ' + fakeTime);
@@ -182,7 +182,7 @@ function generateTest(rdate) {
       'nextHijri': pt[prayerName].hijri,
       'midnightNextDay': mnnext
     };
-  // }
+  }
   });
   
   console.log('result: ' + JSON.stringify(result));
