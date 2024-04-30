47e30e0f5f8fba07fd7dd9815e7ecd0d1564640f
ibnharoon <bustamam@gmail.com>
2024-04-29 18:56:38 -0700

    Update EdgeCases.js

--- test/js/EdgeCases.js
+++ test/js/EdgeCases.js
@@ -219,7 +219,7 @@ for (const date of dates) {
         var netlog = await execAsync('docker network inspect mmm-olprayertime_mm-network');
         console.log('network:');
         console.log(JSON.stringify(netlog));
-        netlog = await execAsync('docker container exec mm-magicmirror ip add');
+        netlog = await execAsync('docker exec mm-magicmirror ifconfig');
         console.log('ip:');
         console.log(JSON.stringify(netlog));
         
