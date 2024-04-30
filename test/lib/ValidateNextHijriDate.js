5dc6f63a160e4f61561444c17ed199e094733435
ibnharoon <bustamam@gmail.com>
2024-04-30 15:41:53 -0700

    Update ValidateNextHijriDate.js

--- test/lib/ValidateNextHijriDate.js
+++ test/lib/ValidateNextHijriDate.js
@@ -3,6 +3,7 @@ const {
 } = require('./ui');
 
 function ValidateNextHijriDate(config) {
+  console.log('validate next hijri:' + JSON.stringify(config));
   it('The next hijri date should be correct', async () => {
     const actualDate = await getElementText(driver, 'ptimeDOM-table-td-date');
     expect(actualDate, `Expected hijri date '${config.nextHijri}' but found '${actualDate}'`).to.equal(config.nextHijri);
