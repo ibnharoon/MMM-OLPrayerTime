2f2170ca33ce2233af1ab6c84fe14d938b45bd2f
ibnharoon <bustamam@gmail.com>
2024-04-29 19:25:17 -0700

    Update EdgeCases.js

--- test/js/EdgeCases.js
+++ test/js/EdgeCases.js
@@ -219,12 +219,12 @@ for (const date of dates) {
         var netlog = await execAsync('docker network inspect mmm-olprayertime_mm-network');
         console.log('network:');
         console.log(JSON.stringify(netlog));
-        // netlog = await execAsync('docker exec mm-magicmirror ip add');
-        // console.log('mm ip:');
-        // console.log(JSON.stringify(netlog));
+        netlog = await execAsync('docker exec mm-magicmirror hostname -i');
+        console.log('mm ip:');
+        console.log(JSON.stringify(netlog));
         
-        netlog = await execAsync('docker exec mm-selenium hostname -l');
-        console.log('selenium network:');
+        netlog = await execAsync('docker exec mm-selenium hostname -i');
+        console.log('selenium ip:');
         console.log(JSON.stringify(netlog));
         
         // save logs from MM and selenium
