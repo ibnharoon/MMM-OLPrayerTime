76ec1986928d8e3950a172a66022f24a88ce795c
ibnharoon <bustamam@gmail.com>
2024-04-30 17:50:00 -0700

    Update ValidateNextPrayerDuration.js

--- test/lib/ValidateNextPrayerDuration.js
+++ test/lib/ValidateNextPrayerDuration.js
@@ -17,7 +17,7 @@ function ValidateNextPrayerDuration(config) {
     var currentDateString = await getElementText(driver, 'currentTime');
     console.log('current date string: ' + currentDateString);
     var endDate = convertTimeStringToDate(prayerTimeString, currentDateString, config.nextPrayer);
-    console.log('end date before: ' + endDate.toDate());
+    console.log('end date before: ' + endDate.toDate() + ', current prayer: ' + config.currentPrayer);
     if (config.currentPrayer === 'Midnight' && endDate.isBefore(this.expectedDate)) {
       console.log('fajr is for yesterday, get today\'s prayer');
       const pt = new PrayerTime(endDate.add(1, 'day').toDate(), 37.3391931, -121.9389783, "en", 12, 'America/Los_Angeles').times;
@@ -28,7 +28,7 @@ function ValidateNextPrayerDuration(config) {
     console.log('midnight before: ' + midnight.toDate());
     const midnighttime = new Dayjs(midnight).add(1, 'day');
     console.log('midnight time: ' + midnighttime.toDate());
-    console.log('midnight: ' + midnight.toDate() + ', enddate: ' + endDate.toDate());
+    console.log('midnight: ' + midnight.toDate() + ', enddate: ' + endDate.toDate() + ', current prayer:' + config.currentPrayer);
     if (config.currentPrayer === 'Midnight' && endDate.isBefore(midnight) && config.midnightNextDay) {
       console.log('add 1 day to enddate');
       endDate = endDate.add(1, 'day');
