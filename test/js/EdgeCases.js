8ca42c33dd0bc41a89ad72284603d1578b9c53ae
ibnharoon <bustamam@gmail.com>
2024-05-01 06:14:29 -0700

    Update EdgeCases.js

--- test/js/EdgeCases.js
+++ test/js/EdgeCases.js
@@ -92,7 +92,6 @@ const stages = [
     blink: true,
     msg: '2m before'
   },
-  /*
   {
     min: 1,
     delay: 60000,
@@ -105,7 +104,6 @@ const stages = [
     blink: false,
     msg: 'on'
   }
-  */
 ];
 
 const dates = [
