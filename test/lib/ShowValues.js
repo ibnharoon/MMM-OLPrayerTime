251e5b2c1be5469ae2d219059e2b3872dc8d9f45
ibnharoon <bustamam@gmail.com>
2024-05-02 06:54:06 -0700

    Update ShowValues.js

--- test/lib/ShowValues.js
+++ test/lib/ShowValues.js
@@ -2,21 +2,23 @@ const {
   getElementText
 } = require('./ui');
 
-function ShowValues(config) {
-  console.log('Show values');
-  const date = await getElementText(driver, 'currentTime');
-  console.log('date: ' + date);
+async function ShowValues(config) {
+  return new Promise(() => {
+    console.log('Show values');
+    const date = await getElementText(driver, 'currentTime');
+    console.log('date: ' + date);
 
-  const hijri = await getElementText(driver, 'ptimeDOM-table-td-date');
-  console.log('hijri: ' + hijri);
+    const hijri = await getElementText(driver, 'ptimeDOM-table-td-date');
+    console.log('hijri: ' + hijri);
 
-  const duration = await getElementText(driver, 'ptimeDOM-premain');
-  console.log('duration: ' + duration);
+    const duration = await getElementText(driver, 'ptimeDOM-premain');
+    console.log('duration: ' + duration);
 
-  for (const prayer of ['Fajr', 'Dhuhr', 'Asr', 'Maghrib', 'Isha']) {
-    const time = await getElementText(driver, 'ptimeDOM-table-td-ptime-' + prayer);
-    console.log(prayer + ': ' + time);
-  }
+    for (const prayer of ['Fajr', 'Dhuhr', 'Asr', 'Maghrib', 'Isha']) {
+      const time = await getElementText(driver, 'ptimeDOM-table-td-ptime-' + prayer);
+      console.log(prayer + ': ' + time);
+    }
+  });
 }
 
 module.exports = {
