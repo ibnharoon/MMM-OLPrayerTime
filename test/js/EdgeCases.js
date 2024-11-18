f11ee0a1615940566a47c3dccdc25d63de810eac
ibnharoon <bustamam@gmail.com>
2024-11-18 14:24:59 -0600

    Update EdgeCases.js

--- test/js/EdgeCases.js
+++ test/js/EdgeCases.js
@@ -228,7 +228,7 @@ for (const date of dates) {
         // var javaver = await execAsync('docker exec mm-selenium java -version');
         // console.log('java version: ' + JSON.stringify(javaver));
 
-        var selsup = await execAsync('docker exec mm-selenium ls -lastr /etc/supervisor/conf.d');
+        var selsup = await execAsync('docker exec mm-selenium cat /etc/supervisor/conf.d/selenium.conf');
         console.log('selenium supervisor config: ' + JSON.stringify(selsup));
 
         var dockrun = await execAsync('docker ps');
